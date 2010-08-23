192480773e0fb48efc44a4e58223fd08e1fb687f
Sylvain DIDELOT <didelot.sylvain@gmail.com>
2010-08-23 23:39:22 +0200

    New functionalities * Navigation with arrows * Doc implementation

---
+++ gui/sdk_colors.h
@@ -0,0 +1,44 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sdk_colors.h
+ *
+ *    Description:  Sudoku resolver - A easy-to-use tool for finding the
+ *                  solution of a Sudoku grid.
+ *
+ *        Created:  23/08/2010 20:54:16
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sylvain Didelot, didelot.sylvain@gmail.com
+ *
+ * =====================================================================================
+ *
+ * Sudoku Resolver - An easy-to-use tool for finding the solution of a
+ * Sudoku grid.
+ * Copyright (C) 2010  Sylvain Didelot - didelot.sylvain@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>."
+ */
+
+/* GTK colors */
+UNUSED static GdkColor c_gray = { 10, 0xaaaa, 0xaaaa, 0xaaaa };
+static GdkColor c_light_gray = { 10, 0xdddd, 0xdddd, 0xdddd };
+UNUSED static GdkColor c_dark_gray = { 10, 0xaaaa, 0xaaaa, 0xaaaa };
+static GdkColor c_red= { 10, 0xffff, 0xaaaa, 0xaaaa};
+static GdkColor c_dark_red= { 10, 0xffff, 0x0000, 0x0000};
+static GdkColor c_black= { 10, 0x0000, 0x0000, 0x0000};
+static GdkColor c_white= { 10, 0xffff, 0xffff, 0xffff};
+static GdkColor c_dark= { 10, 0x6666, 0x6666, 0x6666};
+
